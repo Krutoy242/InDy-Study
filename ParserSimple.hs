f634ff2c86e9828664466af4dbe7b7cba3969d34
Krutoy242 <krutoy242@gmail.com>
2020-05-09 15:09:27 +0300

    lesser operators

--- ParserSimple.hs
+++ ParserSimple.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import InDyModel
+
 -- String wrap for ridicuilous operator name
 wrap = "operator.operators.integrateddynamics.@@@.name"
 wrapReplace = "@@@"
@@ -7,14 +11,9 @@ parseSimple :: String -> operator
 parseSimple = pipe (apply (flip (apply replace wrapReplace)) wrap) opByName
 --parseSimple str = opByName (replace wrapReplace str wrap)
 
--- Simple reduce operator appling [n-1] element to [n] of list
-opReduce :: (a->b) -> String -> c
-opReduce = pipe parseSimple
--- opReduce op s = op(parseSimple s)
-
 -- Parse list of operators
 parseList :: [a] -> operator
-parseList = pipe2 tail (pipe head parseSimple) (reduce opReduce)
+parseList = pipe2 tail (pipe head parseSimple) (reduce (pipe parseSimple))
 -- parseList list = reduce opReduce (tail list) (parseSimple (head list))
 
 -- Parse whole code
